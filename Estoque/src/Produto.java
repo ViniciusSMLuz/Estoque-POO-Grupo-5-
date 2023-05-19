e33a1e6367011ce763a28c35636345a38771ec7f
Gabriel Rozanczyk Ponzio <110409807+WoLF-Ponzio@users.noreply.github.com>
2023-05-19 10:53:42 -0300

    Create Produto

---
+++ Estoque/src/Produto.java
@@ -0,0 +1,31 @@
+import java.util.UUID;
+
+public class Produto {
+    private String idProduto;
+    private String nomeProduto;
+    private int quantidadeProduto;
+
+    public Produto(){
+        idProduto = UUID.randomUUID().toString();
+    }
+
+    public String getIdProduto() {
+        return idProduto;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+    public int getQuantidadeProduto() {
+        return quantidadeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public void setQuantidadeProduto(int quantidadeProduto) {
+        this.quantidadeProduto = quantidadeProduto;
+    }
+
+}
