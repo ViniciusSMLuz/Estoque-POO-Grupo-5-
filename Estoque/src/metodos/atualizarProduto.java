7eb09b9ee09e9ecd6f3c12c50b7d1f1bae4eff57
Andre Kalili <99913120+AndreKalili@users.noreply.github.com>
2023-05-19 11:58:21 -0300

    Foi feito o Atualizar do Produto

---
+++ Estoque/src/metodos/atualizarProduto.java
@@ -0,0 +1,33 @@
+package metodos;
+
+import Classes.*;
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.parseInt;
+import java.util.ArrayList;
+
+public class atualizarProduto {
+
+    public void atualizar(ArrayList<Produto> estoque) {
+        boolean produtoEncontrado = false;
+        String nomeProduto = showInputDialog(null, "Digite o nome do Produto a ser atualizado", "ATUALIZAÇÃO",
+                INFORMATION_MESSAGE);
+
+        for (Produto produto : estoque) {
+            if (produto.getNomeProduto().equals(nomeProduto)) {
+                int novaQuantidade = parseInt(showInputDialog(null, "Digite a nova quantidade disponível no estoque",
+                        "Atualização", INFORMATION_MESSAGE));
+                produto.setQuantidadeProduto(novaQuantidade);
+                showMessageDialog(null, "Quantidade do produto atualizada com sucesso!", "Atualização",
+                        INFORMATION_MESSAGE);
+                produtoEncontrado = true;
+                break;
+            }
+        }
+
+        if (!produtoEncontrado) {
+            showMessageDialog(null, "Produto não encontrado!", "Atualização", ERROR_MESSAGE);
+        } else if (estoque.isEmpty()) {
+            showMessageDialog(null, "Estoque vazio!", "Atualização", ERROR_MESSAGE);
+        }
+    }
+}
