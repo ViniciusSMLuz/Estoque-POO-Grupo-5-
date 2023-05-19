31155c3660453593115340a127e81034bf8725e6
V125-dev <110831331+ViniciusSMLuz@users.noreply.github.com>
2023-05-19 11:37:48 -0300

    alterei algumas mensagens de saída no terminal

--- Estoque/src/EstoqueCozinha.java
+++ Estoque/src/EstoqueCozinha.java
@@ -19,16 +19,16 @@ import Classes.*;
     
         public void atualizarQuantidade(String ingrediente, int novaQuantidade) {
             estoque.put(ingrediente, novaQuantidade);
-            System.out.println("Quantidade atualizada do ingrediente: " + ingrediente);
+            System.out.println("Quantidade do ingrediente atualizada: " + ingrediente);
         }
     
         public boolean verificarDisponibilidade(String ingrediente, int quantidadeRequerida) {
             int quantidadeAtual = estoque.getOrDefault(ingrediente, 0);
             if (quantidadeAtual >= quantidadeRequerida) {
-                System.out.println("Ingrediente disponível em quantidade suficiente: " + ingrediente);
+                System.out.println("Ingrediente disponível em quantidade suficiente para o pedido: " + ingrediente);
                 return true;
             } else {
-                System.out.println("Ingrediente indisponível em quantidade suficiente: " + ingrediente);
+                System.out.println("Ingrediente indisponível em quantidade suficiente para o pedido: " + ingrediente);
                 return false;
             }
         }
@@ -36,22 +36,26 @@ import Classes.*;
         public static void main(String[] args) {
             estoqueCozinha estoque = new estoqueCozinha();
             
-            // Adicionar ingredientes ao estoque
+            // Adicionar ingredientes no estoque
+
             estoque.adicionarIngrediente("Sal", 500);
             estoque.adicionarIngrediente("Açúcar", 1000);
             estoque.adicionarIngrediente("Farinha", 2000);
             
-            // Verificar disponibilidade de ingredientes
+            // Verificar a disponibilidade dos ingredientes
+
             estoque.verificarDisponibilidade("Sal", 200);
             estoque.verificarDisponibilidade("Açúcar", 1500);
             estoque.verificarDisponibilidade("Farinha", 3000);
             
-            // Atualizar quantidade de ingredientes
+            // Atualizar a quantidade de ingredientes
+
             estoque.atualizarQuantidade("Sal", 300);
             estoque.atualizarQuantidade("Açúcar", 500);
             estoque.atualizarQuantidade("Farinha", 1500);
             
-            // Verificar disponibilidade após atualização
+            // Verificar a disponibilidade após atualização
+
             estoque.verificarDisponibilidade("Sal", 200);
             estoque.verificarDisponibilidade("Açúcar", 1500);
             estoque.verificarDisponibilidade("Farinha", 3000);
