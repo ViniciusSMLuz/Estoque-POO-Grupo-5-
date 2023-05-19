ca463aac03d1291e57d3b4d922b1286ba0f16a1b
Gabriel Rozanczyk Ponzio <110409807+WoLF-Ponzio@users.noreply.github.com>
2023-05-19 11:39:36 -0300

    Criação cadastro Produto

--- Estoque/src/metodos/cadastrarProduto.java
+++ Estoque/src/metodos/cadastrarProduto.java
@@ -1,14 +1,39 @@
 package metodos;
 import Classes.*;
 import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.parseInt;
 import java.util.ArrayList;
 
 public class cadastrarProduto {
     
     public Produto cadastrar(ArrayList<Produto> estoque){
-        
-        
-        return null;
+        Produto produto = new Produto();
+        boolean teste = false;
+            while(teste == false){
+                produto.setNomeProduto(showInputDialog(null, "Digite o nome do Produto a ser cadastrado", "CADASTRO", INFORMATION_MESSAGE));
+                    if(!estoque.isEmpty()){
+                        for(Produto produtoTeste : estoque){
+                            if(produtoTeste.getNomeProduto().equals(produto.getNomeProduto())){
+                                showMessageDialog(null, "Produto já Cadastrado!", "Cadastro", ERROR_MESSAGE);
+                                teste = false;
+                            }
+                        }
+                    }else{
+                        teste = true;
+                    }
+                teste = true;
+            }
+
+            teste = false;
+            while(teste == false){
+                try {
+                    produto.setQuantidadeProduto(parseInt(showInputDialog(null, "Digite a quantidade disponível no estoque", "Cadastro", INFORMATION_MESSAGE)));
+                } catch (Exception e) {
+                    showMessageDialog(null, "ERRO: " + e + "\nTente o processo novamente", "Cadastro", ERROR_MESSAGE);            
+                }
+                teste = true;
+            }
+        return produto;
     }
 
 }
