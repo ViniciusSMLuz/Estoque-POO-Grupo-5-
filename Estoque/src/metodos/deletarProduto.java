2c7ac35af58870e1b87cc2dfbc2cde9424457157
tata28t <110830624+tata28t@users.noreply.github.com>
2023-05-19 12:06:27 -0300

    1- parte do deletar

---
+++ Estoque/src/metodos/deletarProduto.java
@@ -0,0 +1,37 @@
+package metodos;
+import Classes.*;
+import static javax.swing.JOptionPane.*;
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+
+
+public class deletarProduto {
+
+    public void deletar(ArrayList<Produto> estoque){
+        
+        String i = showInputDialog("Insira o nome do produto a ser removido");
+
+        for(Produto produto: estoque){
+
+            
+            if(produto.getNomeProduto().equalsIgnoreCase(i)){
+                int escolha = JOptionPane.showConfirmDialog(null, "Produto encontrado! \nTem certeza que gostaria de remover esse produto: " + produto.getNomeProduto(),"Obliteração"  , YES_NO_OPTION);
+                if (escolha == YES_OPTION) {
+                    estoque.remove(produto); 
+                    JOptionPane.showMessageDialog(null, "Produto Excluido com sucesso!", "Confirmação de Exclusão", INFORMATION_MESSAGE);
+                    return;
+                }else if (escolha == JOptionPane.NO_OPTION) {
+                    JOptionPane.showMessageDialog(null, "Cancelando a Exclusão", "Confirmação de Exclusão", INFORMATION_MESSAGE);
+                    return;
+                } else if (escolha == JOptionPane.CLOSED_OPTION) {
+                    JOptionPane.showMessageDialog(null, "Operação encerrada sem escolha! Retornando ao menu", "Confirmação de Exclusão", INFORMATION_MESSAGE);
+                    return;
+                }
+
+
+            }
+       
+    }
+    
+}
+}
